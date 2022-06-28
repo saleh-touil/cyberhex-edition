0c51e4b8582c3a7c719cb0fb55fd68ce2ec32c01
Saleh Eddine Touil <91285170+saleh-touil@users.noreply.github.com>
2022-06-28 14:55:09 +0100

    Add files via upload

---
+++ build/precache-manifest.2293590a96331b59aad2b877927f1cbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6836b62cdd03a6d00e8fec0244729401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3c408dd22c6cbb34b9",
+    "url": "/static/css/main.c585ed2f.chunk.css"
+  },
+  {
+    "revision": "e46ebfc0951f5f999332",
+    "url": "/static/js/2.89807d3f.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.89807d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3c408dd22c6cbb34b9",
+    "url": "/static/js/main.13c38f5e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.13c38f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
